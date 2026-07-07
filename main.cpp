b0af48df6a00c536c6954035aef1c79a970cfc0c
Snesnopic <snesnopic@gmail.com>
2026-07-07 09:34:09 +0200

    Add --version/--help flags, use EXIT_SUCCESS/EXIT_FAILURE

--- main.cpp
+++ main.cpp
@@ -92,9 +92,21 @@ bool unpack(const fs::path& input, const fs::path& output) {
 }
 
 int main(int argc, char* argv[]) {
+    if (argc == 2) {
+        const std::string arg = argv[1];
+        if (arg == "--version") {
+            std::cout << GFTCPP_VERSION << "\n";
+            return EXIT_SUCCESS;
+        }
+        if (arg == "-h" || arg == "--help") {
+            print_usage();
+            return EXIT_SUCCESS;
+        }
+    }
+
     if (argc != 4) {
         print_usage();
-        return 1;
+        return EXIT_FAILURE;
     }
 
     const std::string command = argv[1];
